ca3a94ae1c426e5e95d5944d712926ddadeddd4e
Adam Kentosh <akentosh@gmail.com>
2020-07-01 21:53:32 -0400

    initial add

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "example-server-linuxvm" {
+  source        = "ptfe.this-demo.rocks/akentosh/vm/vsphere"
+  version       = "1.3.0"
+  vmtemp        = "UbuntuTemplate"
+  instances     = 1
+  vmname        = "akentosh-vm-from-module"
+  vmrp          = "Demo-ResourcePool"
+  network_cards = "VM Network"
+  ipv4 = {
+    "VM Network" = ["10.100.0.89"] # To use DHCP create Empty list for each instance
+  }
+  dc        = "PacketDatacenter"
+  datastore = "datastore1"
+}
