d885394f54f1e7090d185cf5c190528b07d2c375
Adam Kentosh <akentosh@gmail.com>
2020-07-01 22:28:59 -0400

    update with new module

--- main.tf
+++ main.tf
@@ -1,17 +1,4 @@
 module "example-server-linuxvm" {
-  source        = "ptfe.this-demo.rocks/akentosh/vm/vsphere"
-  version       = "1.3.0"
-  vmtemp        = "UbuntuTemplate"
-  instances     = 1
-  is_windows_image = "false"
-  domain_admin_user = "Administrator"
-  domain_admin_password = "t3mppass"
-  vmname        = "akentosh-vm-from-module"
-  vmrp          = "Demo-ResourcePool"
-  network_cards = "VM Network"
-  ipv4 = {
-    "VM Network" = ["10.100.0.89"] # To use DHCP create Empty list for each instance
-  }
-  dc        = "PacketDatacenter"
-  datastore = "datastore1"
+  source        = "ptfe.this-demo.rocks/akentosh/virtualmachine/vsphere"
+  version       = "1.0.0"
 }
